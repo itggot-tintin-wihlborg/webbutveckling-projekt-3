813fda12278293217b45d30bc48c2ae75ba3bcf6
itggot-tintin-wihlborg <tintin.wihlborg@elev.ga.ntig.se>
2019-03-14 15:43:07 +0100

    cart site transition

--- site/JS/main.js
+++ site/JS/main.js
@@ -3,4 +3,21 @@ function menu(event){
     let menu = document.querySelector(".menu");
     menu.classList.toggle("show")
 
+    let shadow = document.querySelector(".shadow")
+    shadow.classList.toggle("showshadow")
+
+}
+
+function cart(event){
+
+
+
+    let cart = document.querySelector(".cart")
+    cart.classList.toggle("showcart")
+
+    
+    let cartwrapper = document.querySelector(".cart-wrapper")
+    cartwrapper.classList.toggle("show-cart-wrapper")
+
+
 }
