616a849bf285c7a71d479737f50ed92fb0c8b8c5
itggot-tintin-wihlborg <tintin.wihlborg@elev.ga.ntig.se>
2019-03-21 15:36:28 +0100

    info och item site

--- site/JS/main.js
+++ site/JS/main.js
@@ -21,3 +21,43 @@ function cart(event){
 
 
 }
+function cart2(event){
+
+
+    let menu = document.querySelector(".menu");
+    menu.classList.toggle("show")
+
+    let shadow = document.querySelector(".shadow")
+    shadow.classList.toggle("showshadow")
+
+    let main = document.querySelector("#cart")
+
+    if (main.classList.contains('showcart') == false){
+        let cart = document.querySelector(".cart")
+        cart.classList.toggle("showcart")
+
+        
+        let cartwrapper = document.querySelector(".cart-wrapper")
+        cartwrapper.classList.toggle("show-cart-wrapper")
+    }
+}
+function home(event){
+
+    let menu = document.querySelector(".menu");
+    menu.classList.toggle("show")
+
+    let shadow = document.querySelector(".shadow")
+    shadow.classList.toggle("showshadow")
+
+    let main = document.querySelector("#cart")
+
+    if (main.classList.contains('showcart') == true){
+        let cart = document.querySelector(".cart")
+        cart.classList.toggle("showcart")
+
+        
+        let cartwrapper = document.querySelector(".cart-wrapper")
+        cartwrapper.classList.toggle("show-cart-wrapper")
+    }
+
+}
